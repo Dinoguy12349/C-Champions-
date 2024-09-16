b6ef9e78d9980d180f038a81afe6d6b16765fda3
Yahia <yahia.nawashi@gmail.com>
2024-09-16 18:07:31 +0300

    added horribly unoptimized code(3rd question)

---
+++ Introduction/icouldntuseforlloopforthisone.cpp
@@ -0,0 +1,9 @@
+// Write Your school name for 5 times.
+#include <iostream>
+int main(){
+  std::cout <<"SIA"<< std::endl;
+  std::cout <<"SIA"<< std::endl;
+  std::cout <<"SIA"<< std::endl;
+  std::cout <<"SIA"<< std::endl;
+  std::cout <<"SIA"<< std::endl;
+}
