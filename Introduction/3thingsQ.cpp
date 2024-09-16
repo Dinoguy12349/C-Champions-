65ad4268b83e8a9c6ddf2c8f3d7f252ecd6c731b
Yahia <yahia.nawashi@gmail.com>
2024-09-16 18:04:26 +0300

    added 3 lists question file

---
+++ Introduction/3thingsQ.cpp
@@ -0,0 +1,7 @@
+// Create a program that prints a list of 3 things you like.
+#include <iostream>
+int main(){
+  std::cout <<"gaming"<< std::endl;
+  std::cout <<"reading"<< std::endl;
+  std::cout <<"sleeping"<< std::endl;
+}
