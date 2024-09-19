2308a682d815ec1ebda329ab596ca0ce1a41e255
Yahia <yahia.nawashi@gmail.com>
2024-09-19 18:24:47 +0300

    added question and answer into addof2num file

--- variables/additionof2num.cpp
+++ variables/additionof2num.cpp
@@ -4,4 +4,6 @@ int main(){
     int num1=5;
     int num2=6;
     cout<<num1+num2<<endl;
+    // Print 6*3 in output, dont print 18, just 6*3 should be in the output screen.
+    cout<<"6*3"<<endl;
 }
