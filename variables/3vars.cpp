5d70f8a105f55dabad0109fc83854e2c4155146a
Yahia <yahia.nawashi@gmail.com>
2024-09-23 17:16:54 +0300

    added 3vars.cpp

---
+++ variables/3vars.cpp
@@ -0,0 +1,10 @@
+
+// Assignment : Write a C++ program that declares three variables: an integer, a float, and a character. Initialize them with values and print them to the console.
+#include <iostream>
+using namespace std;
+int main(){
+    int var1=5;
+    bool var2=false;
+    string var3="hello";
+    cout<<var1<<" "<<var2<<" "<<var3<<endl;
+}
