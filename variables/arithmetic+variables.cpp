bdf96277257c43d8d1fcbaf19eeab04f92234579
Yahia <yahia.nawashi@gmail.com>
2024-09-29 19:33:09 +0300

    added multiple files

---
+++ variables/arithmetic+variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=5;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<"6*3"<<endl;
+    int var1=6;
+    int var2=7;
+    int hold;
+    cout<<var1<<" "<<var2<<endl;
+    hold=var1;
+    var1=var2;
+    var2=hold;
+    cout<<var1<<" "<<var2<<endl;
+    
+}
---
+++ variables/typeconversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // float var1=1.5;
+    // cout<<var1<<endl;
+    // int var1;
+    // cout<<var1<<endl;
+    // int var2 = 6;
+    // cout<<var2<<endl;
+    // float var2;
+    // cout<<var2<<endl;
+
+}
