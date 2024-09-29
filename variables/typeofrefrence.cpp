7caab87c1f396f1c884db016a89c8fd17d968e5a
Yahia <yahia.nawashi@gmail.com>
2024-09-29 19:59:38 +0300

    added refrence file for typeof

---
+++ variables/typeofrefrence.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Integer
+    int a=20;
+    cout<<a<<endl;
+
+    // float
+    float f=20.5;
+    cout<<f<<endl;
+
+    // char
+    char ch='K';
+    cout<<ch<<endl;
+
+    // double
+
+    double d=20;
+
+ // string
+ string str ="Hello";
+
+
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+cout << "Size of string: " << sizeof(str) << " bytes" << endl;
+
+
+}
